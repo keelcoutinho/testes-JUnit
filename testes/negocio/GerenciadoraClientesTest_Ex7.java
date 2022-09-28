1c2f3c5c4caad7e8e8b7a7ec42a1655fec968a94
keelcoutinho <kelvia_cou@hotmail.com>
2022-09-28 17:10:37 -0300

    First commit

---
+++ testes/negocio/GerenciadoraClientesTest_Ex7.java
@@ -0,0 +1,85 @@
+package negocio;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Classe de teste criada para garantir o funcionamento das principais operações
+ * sobre clientes, realizadas pela classe {@link GerenciadoraClientes}.
+ * 
+ * @author keelcoutinho
+ *  
+ */
+public class GerenciadoraClientesTest_Ex7 {
+
+	private GerenciadoraClientes gerClientes;
+	private int idCLiente01 = 1;
+	private	int idCLiente02 = 2;
+	
+	@Before
+	public void setUp() {
+	
+		/* ========== Montagem do cenário ========== */
+		
+		// criando alguns clientes
+		Cliente cliente01 = new Cliente(idCLiente01, "Ana Coutinho", 28, "anacout@gmail.com", 1, true);
+		Cliente cliente02 = new Cliente(idCLiente02, "Laura Sofia", 20, "lauras@gmail.com", 1, true);
+		
+		// inserindo os clientes criados na lista de clientes do banco
+		List<Cliente> clientesDoBanco = new ArrayList<>();
+		clientesDoBanco.add(cliente01);
+		clientesDoBanco.add(cliente02);
+		
+		gerClientes = new GerenciadoraClientes(clientesDoBanco);
+	
+	}
+
+	@After
+	public void tearDown() {
+		gerClientes.limpa();
+	}
+	
+	/**
+	 * Teste básico da pesquisa de um cliente a partir do seu ID.
+	 * 
+	 * @author keelcoutinho
+	 * 
+	 */
+	@Test
+	public void testPesquisaCliente() {
+
+		/* ========== Execução ========== */
+		Cliente cliente = gerClientes.pesquisaCliente(idCLiente01);
+		
+		/* ========== Verificações ========== */
+		assertThat(cliente.getId(), is(idCLiente01));
+		
+	}
+	
+	/**
+	 * Teste básico da remoção de um cliente a partir do seu ID.
+	 * 
+	 * @author keelcoutinho
+	 * 
+	 */
+	@Test
+	public void testRemoveCliente() {
+		
+		/* ========== Execução ========== */
+		boolean clienteRemovido = gerClientes.removeCliente(idCLiente02);
+		
+		/* ========== Verificações ========== */
+		assertThat(clienteRemovido, is(true));
+		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
+		assertNull(gerClientes.pesquisaCliente(idCLiente02));
+		
+	}
+	
+}
